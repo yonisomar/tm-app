44346ffdd03b7554f368817bc3a878a859a1ecfb
yonisomar <yonisomar007@gmail.com>
2025-04-22 00:55:00 +0100

    fmt backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "threat-modeling-app--tfstate"
-    key    = "state"
-    region = "us-east-1"
-    encrypt = true
+    bucket       = "threat-modeling-app--tfstate"
+    key          = "state"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
   }
+
 }
